0b28f3ba1a3f606c83b032c5c19cd8fa032b10d2
Aryan Jain <52525883+Aryan-Jain-1710@users.noreply.github.com>
2022-07-18 14:18:53 -0400

    Update filechar.c

--- filechar.c
+++ filechar.c
@@ -2,14 +2,27 @@
 // *** You MUST modify this file
 // ***
 
-#include <stdio.h>
-#include <stdbool.h>
 
+#include <stdio.h>
+#include <stdlib.h>
 
 #ifdef TEST_PRINT
-void print(char * filename)
-{
+void print(char * filename) {
   // Print the text in the given file
+
+  FILE * fp = fopen(filename, "r");
+
+  if(fp == NULL)
+  {
+      return;
+  }
+
+  char * buff;
+
+  fgets(buff, 100, fp);
+  printf("%s\n", buff);
+
+  fclose(fp);
 
 }
 #endif
